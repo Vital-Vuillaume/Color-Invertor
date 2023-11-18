f33766d5fb92b8f27247250b12e3c6141997996c
Vital Vuillaume <vitalvuillaume@proton.me>
2023-11-18 21:23:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,21 +8,22 @@ def on_image_click(event):
 def on_image_hover(event):
     canvas.config(cursor="hand2")
     canvas2.config(cursor="hand1")
-    canvas3.config(cursor="hand1")
 
 def on_image_leave(event):
     canvas.config(cursor="")
     canvas2.config(cursor="")
-    canvas3.config(cursor="")
 
 def toggle_image2_visibility(event=None):
+    global canvas2
+    global label_titre
     current_width = app.winfo_width()
     if current_width >= 1700:
+        canvas2.config(width=350, height=350)
+        label_titre.config(font=(global_font, 34))
         canvas2.pack(side=tk.LEFT)
-        canvas3.pack_forget()
     else:
-        canvas3.pack()
-        canvas2.pack_forget()
+        canvas2.config(width=300, height=300)
+        label_titre.config(font=(global_font, 16))
 
 app = tk.Tk()
 app.title("Ma première application")
@@ -36,10 +37,10 @@ if platform.system() == "Windows":
 
 app.attributes('-zoomed', True)
 
-app.minsize(width=700, height=400)
+app.minsize(width=975, height=700)
 
 frame = tk.Frame(app, bg="#1a1a30")
-frame.pack(expand=True, fill=tk.BOTH, padx=100)
+frame.pack(expand=True, fill=tk.BOTH, padx=90)
 
 canvas = Canvas(frame, bg="#1a1a30", width=300, height=320, highlightthickness=0, bd=0)
 canvas.pack()
@@ -53,7 +54,7 @@ frame_title_canvas2 = tk.Frame(frame, bg="#1a1a30")
 frame_title_canvas2.pack()
 
 titre = "Discover the Beauty of Black and White: Your images take on a new life without color."
-label_titre = tk.Text(frame_title_canvas2, wrap=tk.WORD, width=40, height=5, bg="#1a1a30", highlightthickness=0, bd=0, fg="white", font=(global_font, 36))
+label_titre = tk.Text(frame_title_canvas2, wrap=tk.WORD, width=40, height=5, bg="#1a1a30", highlightthickness=0, bd=0, fg="white", font=(global_font, 34))
 label_titre.insert(tk.END, titre)
 label_titre.pack(side=tk.LEFT)
 
@@ -63,13 +64,6 @@ image_button2 = canvas2.create_image(0, 0, anchor=tk.NW, image=image2, tags="img
 canvas2.tag_bind("img2", '<Enter>', on_image_hover)
 canvas2.tag_bind("img2", '<Leave>', on_image_leave)
 canvas2.tag_bind("img2", '<Button-1>', on_image_click)
-
-canvas3 = Canvas(frame, bg="#1a1a30", width=300, height=320, highlightthickness=0, bd=0)
-image3 = PhotoImage(file="image2.png")
-image3_button = canvas3.create_image(25, 60, anchor=tk.NW, image=image3, tags="img3")
-canvas3.tag_bind("img3", '<Enter>', on_image_hover)
-canvas3.tag_bind("img3", '<Leave>', on_image_leave)
-canvas3.tag_bind("img3", '<Button-1>', on_image_click)
 
 app.bind("<Configure>", toggle_image2_visibility)
 
